e3532ea504e26df45e88653eddaac1ad06737274
V-nsh <vaprohit707@gmail.com>
2023-09-30 14:07:54 +0530

    routing

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
 
         {/* projects */}
         <a className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-        href="/projects"
+        href="/V-nsh/projects"
         >
           <h2 className="mb-3 text-2xl font-semibold">
           Projects{' '}<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -32,7 +32,7 @@ export default function Home() {
 
         {/* contact */}
         <a className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-        href="/contact"
+        href="/V-nsh/contact"
         >
           <h2 className="mb-3 text-2xl font-semibold">
         Contact{' '}<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
